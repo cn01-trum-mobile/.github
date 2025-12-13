d79ee4bcfb1ddcda74f3dd03dee589b7ff7c289f
vck2004 <52705258+vck2004@users.noreply.github.com>
2025-12-13 15:06:30 +0700

    fix picture and logo and link bug

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
 // script.js
 
-document.addEventListener("DOMContentLoaded", () => {
-	console.log("Trang web đã tải xong!");
-
-	// Ví dụ về một tính năng nhỏ: highlight menu khi cuộn
-	const sections = document.querySelectorAll("section");
-	const navLinks = document.querySelectorAll(".nav-link");
-
-	window.addEventListener("scroll", () => {
-		let current = "";
-		sections.forEach((section) => {
-			const sectionTop = section.offsetTop;
-			if (scrollY >= sectionTop - 60) {
-				current = section.getAttribute("id");
-			}
-		});
-
-		navLinks.forEach((link) => {
-			link.classList.remove("active");
-			if (link.href.includes(current)) {
-				link.classList.add("active");
-			}
-		});
-	});
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   const l1 = document.getElementById('l1');
+//   const l2 = document.getElementById('l2');
+//   console.log();
+//   if (window.location.href.includes('product')) l2.classList.add('active');
+//   else l1.classList.add('active');
+// });
